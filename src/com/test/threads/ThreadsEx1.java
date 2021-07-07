9b41297a17347094216f566d312c24fa7d9145dd
Amit Kaushik Sharma <amit22feb@gmail.com>
2021-07-07 15:20:13 +0530

    First commit for Thread program

---
+++ src/com/test/threads/ThreadsEx1.java
@@ -0,0 +1,51 @@
+package com.test.threads;
+
+public class ThreadsEx1{
+
+	private String threadName;
+	public ThreadsEx1(String name){
+		
+	}
+	public static void main(String[] args) {
+	
+		Thread t1 = new Thread(new MyThread("Thread-1"));
+		Thread t2 = new Thread(new MyThread("Thread-2"));
+		t1.start();
+		t2.start();
+		for(int i=0;i<5;i++){
+			System.out.println("Main thread: "+i);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+}
+
+class MyThread implements Runnable{
+
+	private String threadName;
+	public MyThread(String threaName) {
+		this.threadName = threaName;
+		System.out.println("Creating: "+ threaName);
+	}	
+	
+	@Override
+	public void run() {
+		System.out.println("Running "+ threadName);
+		
+		for(int i=0;i<4;i++){
+			System.out.println("Thread: "+threadName+" ,"+i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println("Thread "+threadName+" interrupted");
+			}
+		}
+		System.out.println("Thread "+threadName+" exitting");
+	}
+	
+}
